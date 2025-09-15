3a9384f1c8e888c681702b7cfaffce1a036cb084
Sehan Madusanka <sehanm@icasl.com>
2025-09-15 19:32:31 +0530

    Comment changed

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,7 +1,7 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
+    <!-- Page Heading 1 -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
